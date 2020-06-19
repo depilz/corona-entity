18161bcfe6ea1bc3a642909f8f118047808c9ac6
depilz <clarospilzdw@gmail.com>
2020-06-19 11:09:32 -0300

    Add MIT license

--- entity.lua
+++ entity.lua
@@ -1,3 +1,33 @@
+local middleclass = {
+  _VERSION     = 'Entity v1.0.0',
+  _DESCRIPTION = 'A basic entity class designed for Solar2D',
+  _URL         = 'https://github.com/depilz/corona-entity',
+  _LICENSE     = [[
+    MIT LICENSE
+
+    Copyright (c) 2020 Denis Claros Pilz
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ]]
+}
+
 -- Helpers
 local function groupGetX(group, anchor, world)
  return group.object:getX(anchor, world)
